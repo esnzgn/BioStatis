5c636d8cd2361a8a548d4f9d98d675408c67718a
Zangene <ehzange@lm1-304-24064.local>
2025-03-29 15:09:48 +0200

    FIRST commit

---
+++ ui.R
@@ -0,0 +1,13 @@
+#
+# This is the user-interface definition of a Shiny web application. You can
+# run the application by clicking 'Run App' above.
+#
+# Find out more about building applications with Shiny here:
+#
+#    https://shiny.posit.co/
+#
+
+ui <- fluidPage(
+  # UI Output
+  textOutput("textOut_tmp")
+)
