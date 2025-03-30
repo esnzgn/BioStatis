cd6104436516e4e399afe405edab6cec378e43e3
Zangene <ehzange@lm1-304-24064.local>
2025-03-30 12:19:22 +0300

    srv per vs boot calc

--- server.R
+++ server.R
@@ -6,24 +6,56 @@
 #
 #    https://shiny.posit.co/
 #
-
-library(shiny)
-
+source("./sys_req.R")
 # Define server logic required to draw a histogram
 function(input, output, session) {
-
-    output$textOut_tmp <- renderText({
-
-        # # generate bins based on input$bins from ui.R
-        # x    <- faithful[, 2]
-        # bins <- seq(min(x), max(x), length.out = input$bins + 1)
-        # 
-        # # draw the histogram with the specified number of bins
-        # hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             # xlab = 'Waiting time to next eruption (in mins)',
-             # main = 'Histogram of waiting times')
-      "permutation vs boostrap simple explanation for everone"
-
+    
+    observeEvent(input$go, {
+      set.seed(123)
+      group1 <- rnorm(input$n, mean = 0, sd = 1)
+      group2 <- rnorm(input$n, mean = input$mean_diff, sd = 1)
+      
+      original_diff <- mean(group2) - mean(group1)
+      combined <- c(group1, group2)
+      
+      # Permutation Test
+      perm_diffs <- replicate(input$n_perm, {
+        shuffled <- sample(combined)
+        mean(shuffled[1:input$n]) - mean(shuffled[(input$n + 1):(2*input$n)])
+      })
+      
+      output$perm_plot <- renderPlot({
+        ggplot(data.frame(perm_diffs), aes(x = perm_diffs)) +
+          geom_histogram(color = "black", fill = "skyblue", bins = 30) +
+          geom_vline(xintercept = original_diff, color = "red", linetype = "dashed") +
+          labs(title = "Permutation Distribution", x = "Mean Differences", y = "Frequency")
+      })
+      
+      p_value <- mean(abs(perm_diffs) >= abs(original_diff))
+      
+      output$perm_pvalue <- renderPrint({
+        cat("Permutation Test P-value:\n")
+        cat(round(p_value, 4))
+      })
+      
+      # Bootstrapping
+      boot_diffs <- replicate(input$n_perm, {
+        g1_boot <- sample(group1, replace = TRUE)
+        g2_boot <- sample(group2, replace = TRUE)
+        mean(g2_boot) - mean(g1_boot)
+      })
+      
+      output$boot_plot <- renderPlot({
+        ggplot(data.frame(boot_diffs), aes(x = boot_diffs)) +
+          geom_histogram(color = "black", fill = "orange", bins = 30) +
+          geom_vline(xintercept = quantile(boot_diffs, c(0.025, 0.975)), color = "red", linetype = "dashed") +
+          labs(title = "Bootstrap Distribution", x = "Mean Differences", y = "Frequency")
+      })
+      
+      output$boot_ci <- renderPrint({
+        ci <- quantile(boot_diffs, c(0.025, 0.975))
+        cat("Bootstrapped 95% Confidence Interval:\n")
+        print(round(ci, 4))
+      })
     })
-
-}
+  }
