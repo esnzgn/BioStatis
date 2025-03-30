ee43a39b324a866837187213157c35eaf1db7eff
Zangene <ehzange@lm1-304-24064.local>
2025-03-30 12:19:55 +0300

    disp per boots

--- ui.R
+++ ui.R
@@ -7,9 +7,31 @@
 #    https://shiny.posit.co/
 #
 
-library(shiny)
 
-ui <- fluidPage(
-  # UI Output
-  textOutput("textOut_tmp")
+fluidPage(
+  titlePanel("Permutation Test & Bootstrapping"),
+  
+  sidebarLayout(
+    sidebarPanel(
+      sliderInput("n", "Sample Size per Group:", min = 10, max = 100, value = 30),
+      numericInput("mean_diff", "True Mean Difference:", value = 1, step = 0.1),
+      numericInput("n_perm", "Number of Permutations:", value = 1000, step = 100),
+      actionButton("go", "Run Simulation")
+    ),
+    
+    mainPanel(
+      tabsetPanel(
+        tabPanel("Permutation Test",
+                 br(),
+                 p("This app demonstrates the difference between permutation testing and bootstrapping."),
+                 plotOutput("perm_plot"),
+                 br(),
+                 plotOutput("boot_plot"),
+                 br(),
+                 verbatimTextOutput("perm_pvalue"),
+                 verbatimTextOutput("boot_ci")
+        )
+      )
+    )
+  )
 )
